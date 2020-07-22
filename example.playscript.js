c417382f2ade68ef27a55569d44cabd9dab01cd0
Tyler Abbott <tyler@webspecdesign.com>
2020-07-22 13:18:45 -0500

    get scanner working locally

--- example.playscript.js
+++ example.playscript.js
@@ -3,20 +3,20 @@ const {
 } = require("./lib/page");
 
 const steps = [
-  {title: "Search", async exec(page){
-    await goto(page, "https://www.wikipedia.org/");
+  /* {title: "Search", async exec(page){
+    await goto(page, "https://www.webspecdesign.com/");
     const input = await page.waitForSelector("#searchInput");
     await input.type("");
     await Promise.all([
       page.waitForNavigation(),
       input.evaluate(node => node.form.submit()),// Submit
     ]);
-  }},
-  "https://en.wikipedia.org/wiki/Internet_privacy#HTTP_cookies",
+  }}, */
+  "https://www.webspecdesign.com/",
   "about:blank",// be sure the last page trigger beforeunload event and start requests (keep lives, or synchronous XHR). See https://fetch.spec.whatwg.org/#keep-alive-flag
 ];
 
 module.exports = {
   steps,
-  firstPartyDomain: "wikipedia.org",
+  firstPartyDomain: "webspecdesign.com",
 };
